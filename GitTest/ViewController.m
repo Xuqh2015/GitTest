91c273030e116127335a1766ded772947e268db6
xu <apple@appledeiMac>
2018-04-28 15:06:58 +0800

    test2

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -18,12 +18,15 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [self sayhello];
+    [self sayByebye];
 }
 
 -(void)sayhello{
     NSLog(@"hello");
 }
-
+-(void)sayByebye{
+    NSLog(@"sayByebye");
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
