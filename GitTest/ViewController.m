cb0cdf6f9fe1252be2e36f4667c89983d7142f39
xu <apple@appledeiMac>
2018-04-28 14:34:55 +0800

    first

---
+++ GitTest/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by apple on 2018/4/28.
+//  Copyright © 2018年 xu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [self sayhello];
+}
+
+-(void)sayhello{
+    NSLog(@"hello");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
